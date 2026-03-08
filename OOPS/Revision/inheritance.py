e96756a7b3d1b002d74bacd3531a48c3b90c74b8
prachi07042004 <prachi.07sharma.07@gmail.com>
2026-03-08 07:34:38 +0530

    Implement polymorphism in inheritance file

--- OOPS/Revision/inheritance.py
+++ OOPS/Revision/inheritance.py
@@ -3,6 +3,12 @@
   changes made in parent class automatically reflect in child classes
 
   The super keyword in inheritance is a reference variable used to access the immediate parent class's members (methods, fields, and constructors) from a subclass, promoting code reusability and resolving naming conflicts. It is commonly used to invoke parent constructors (super()) or override methods (super.method()).
+
+  Polymorphism - Poly(many), Morphs(forms) - allows same method name to behave differently based on which object calls it 
+  Same method name works differntly for differnt classes 
+  You don't need to remember different multiple method names for similar actions
+  Method Overriding - child class provides it's own version of a parent's method
+  Ex: a draw() method on a circle draws a circle, on a rectangle it draws a rectangle-same name different behavior
 '''
 
 class Animal:
@@ -17,6 +23,9 @@ class Animal:
   def sleep(self):
     print("I am sleeping")
 
+  def move(self):
+    print("I am moving")
+
 class Dog(Animal):
 
   def __init__(self, name: str, age: int, breed: str):
@@ -29,10 +38,11 @@ class Dog(Animal):
   def display(self):
     print(f"Name is {self.name}, Age is {self.age}")
 
+  def move(self):
+    print("I move on 4 legs")
 
 dog = Dog("Whiskey", 4, "labrador")
 dog.eat()
 dog.bark()
 dog.display()
-
-  
+dog.move() #calls move of dog
